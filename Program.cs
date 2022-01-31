33f813cc7152d99aad93d025e8c67b269aef690c
Marcos Nascimento <marcosnascimento.dev@gmail.com>
2022-01-31 08:53:26 -0300

    crud

---
+++ Program.cs
@@ -0,0 +1,192 @@
+﻿using Dapper;
+using DataAccessDapper.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccessDapper
+{
+    class Program
+    {
+        const string connectionString = "Server=localhost;Database=crud;Integrated Security=True;TrustServerCertificate=True";
+        public static void Main(string[] args)
+        {
+
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Gerenciar Contatos");
+            Console.WriteLine("[C] Criar novo Contato");
+            Console.WriteLine("[R] Listar todos Contatos");
+            Console.WriteLine("[U] Atualizar Contato");
+            Console.WriteLine("[D] Deletar Contato");
+            Console.WriteLine("[E] Sair");
+
+            var option = Console.ReadLine().ToUpper();
+            int id;
+            switch (option)
+            {
+                case "C":
+                    Create();
+                    break;
+                case "R":
+                    Read();
+                    break;
+                case "U":
+                    Console.WriteLine("Informe o ID do Contato");
+                    id = int.Parse(Console.ReadLine());
+                    var contact = UpdateModel();
+                    Update(id, contact);
+                    break;
+                case "D":
+                    Console.WriteLine("Informe o ID do Contato");
+                    id = int.Parse(Console.ReadLine());
+                    Delete(id);
+                    break;
+                case "E":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        public static void Create()
+        {
+
+            var contact = new Contact();
+
+            Console.WriteLine("Cadastro de Contatos");
+            Console.WriteLine("Informe o Nome do Contato: ");
+            contact.Name = Console.ReadLine();
+            Console.WriteLine("Infome o Email do Contato ");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Informe o Telefone do Contato");
+            contact.Phone = Console.ReadLine();
+            Console.WriteLine("Informe a data de nascimento do Contato");
+            var birthDateString = Console.ReadLine();
+            contact.Birthdate = DateTime.Parse(birthDateString);
+            contact.CreateDate = DateTime.Now;
+
+            Save(contact);
+        }
+
+        public static void Save(Contact contact)
+        {
+            string insert = @"INSERT INTO Contact (
+                                        Name,
+                                        Email, 
+                                        Phone,
+                                        Birthdate,
+                                        CreateDate
+                                    )
+                                     VALUES(
+                                         @Name,
+                                         @Email,
+                                         @Phone,
+                                         @Birthdate,
+                                         @CreateDate
+                                    )";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var rows = connection.Execute(insert, new
+                {
+                    contact.Name,
+                    contact.Email,
+                    contact.Phone,
+                    contact.Birthdate,
+                    contact.CreateDate
+                });
+
+                RetornaLinhas(rows, "inserida");
+            }
+
+        }
+
+        public static void Read()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                IEnumerable<Contact> contacts = connection.Query<Contact>("SELECT * FROM Contact");
+                int rows = 0;
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact.Id);
+                    Console.WriteLine(contact.Name);
+                    Console.WriteLine(contact.Email);
+                    Console.WriteLine(contact.Phone);
+                    Console.WriteLine(contact.Birthdate);
+                    Console.WriteLine(contact.CreateDate);
+                    rows++;
+                }
+
+                RetornaLinhas(rows, "retornada");
+            }
+        }
+
+        public static void Update(int id, Contact contact)
+        {
+            string update = @" UPDATE Contact
+                                SET Name = @Name,
+                                    Email = @Email,
+                                    Phone = @Phone
+                                WHERE Id = @Id
+                             ";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var rows = connection.Execute(update, new
+                {
+                    contact.Name,
+                    contact.Email,
+                    contact.Phone,
+                    id,
+                });
+
+                RetornaLinhas(rows, "atualizada");
+            }
+
+        }
+
+        public static Contact UpdateModel()
+        {
+            var contact = new Contact();
+
+            Console.WriteLine("Atualizar dados do Contato");
+            Console.WriteLine("Informe o Nome do Contato: ");
+            contact.Name = Console.ReadLine();
+            Console.WriteLine("Infome o Email do Contato ");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Informe o Telefone do Contato");
+            contact.Phone = Console.ReadLine();
+
+            return contact;
+        }
+        public static void Delete(int id)
+        {
+            string delete = "DELETE FROM Contact WHERE Id = @id";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var rows = connection.Execute(delete, new
+                {
+                    id
+                });
+
+                RetornaLinhas(rows, "delatada");
+            }
+
+        }
+
+        public static void RetornaLinhas(int qtdLinhas, string acao)
+        {
+            if (qtdLinhas == 1)
+            {
+                Console.WriteLine($"{qtdLinhas} linha {acao}");
+            }
+            else
+            {
+                Console.WriteLine($"{qtdLinhas} linhas {acao}s");
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,95 @@
+<div align="center" id="top"> 
+  <!-- <img src="./.github/app.gif" alt="DataAccessDapper" /> -->
+
+&#xa0;
+
+  <!-- <a href="https://dataaccessdapper.netlify.com">Demo</a> -->
+</div>
+
+<h1 align="center">DataAccess Dapper</h1>
+
+<p align="center">
+  <img alt="Principal linguagem do projeto" src="https://img.shields.io/github/languages/top/MarcosNasc/dataaccessdapper?color=56BEB8">
+
+  <img alt="Quantidade de linguagens utilizadas" src="https://img.shields.io/github/languages/count/MarcosNasc/dataaccessdapper?color=56BEB8">
+
+  <img alt="Tamanho do repositório" src="https://img.shields.io/github/repo-size/MarcosNasc/dataaccessdapper?color=56BEB8">
+
+  <img alt="Licença" src="https://img.shields.io/github/license/MarcosNasc/dataaccessdapper?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/MarcosNasc/dataaccessdapper?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/MarcosNasc/dataaccessdapper?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/MarcosNasc/dataaccessdapper?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center">
+	🚧  DataAccessDapper 🚀 Em construção...  🚧
+</h4>
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-sobre">Sobre</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-funcionalidades">Funcionalidades</a> &#xa0; | &#xa0;
+  <a href="#rocket-tecnologias">Tecnologias</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-pré-requesitos">Pré requisitos</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-começando">Começando</a> &#xa0; | &#xa0;
+  <a href="#memo-licença">Licença</a> &#xa0; | &#xa0;
+  <a href="https://github.com/MarcosNasc" target="_blank">Autor</a>
+</p>
+
+<br>
+
+## :dart: Sobre
+
+Aplicação Console com operações de CRUD utilizando SQL SERVER e Dapper.
+
+## :sparkles: Funcionalidades
+
+:heavy_check_mark: CREATE\
+:heavy_check_mark: READ\
+:heavy_check_mark: UPDATE\
+:heavy_check_mark: DELETE
+
+## :rocket: Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+
+- [.NET C#](https://docs.microsoft.com/pt-br/dotnet/csharp/tour-of-csharp/)
+- [Dapper](https://dapper-tutorial.net/dapper)
+
+## :white_check_mark: Pré requisitos
+
+Antes de começar :checkered_flag:, você precisa ter o [Git](https://git-scm.com) e o [SQL SERVER](https://dotnet.microsoft.com/en-us/download) instalados em sua maquina.
+
+## :checkered_flag: Começando
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/MarcosNasc/dataaccessdapper
+
+# Entre na pasta
+$ cd dataaccessdapper
+
+# Abra no visual studio code
+$ code .
+
+# Para iniciar o projeto
+$ dotnet run
+
+# O app vai inicializar no terminal
+```
+
+## :memo: Licença
+
+Este projeto está sob licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+Feito com :heart: por <a href="https://github.com/MarcosNasc" target="_blank">Marcos Nascmento</a>
+
+&#xa0;
+
+<a href="#top">Voltar para o topo</a>
